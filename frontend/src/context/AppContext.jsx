091ff7a77d6b3e9269a694cedd2c1c730b3d5d6e
Arjun Singh Chilwal <119243879+arjunchilwal112@users.noreply.github.com>
2024-12-12 04:28:32 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,7 +10,7 @@ const AppContextProvider=(props)=>{
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
 const currencySymbol='$'
-const backendUrl="https://prescripto-backend-arjun.onrender.com"
+const backendUrl= "https://prescripto-backend-arjun.onrender.com"
 const [userData, setuserData] = useState(false)
 
 
