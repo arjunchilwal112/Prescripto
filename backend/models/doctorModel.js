d649db4f7f2a4fb75bac33f3c509e61243ad6d46
bhattproject <bhattjirajeev@gmail.com>
2025-05-28 12:24:01 +0530

    Update doctorModel.js

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -12,8 +12,7 @@ const doctorSchema = new mongoose.Schema({
     available: { type: Boolean, default: true },
     fees: { type: String, required: true },
     address: { type: Object, required: true },
-    date: { type: Number, required: true },
-    slots_booked: { type: Object, default: {} }
+   
 }, { minimize: false })
 
 
