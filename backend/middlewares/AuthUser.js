aa20849811105fc670175f5416b8198f62528538
mansifartiyal <mansifartiyal8@gmail.com>
2025-05-28 12:03:11 +0530

    the bug is resolved

--- backend/middlewares/AuthUser.js
+++ backend/middlewares/AuthUser.js
@@ -10,7 +10,10 @@ const AuthUser =async (req,res,next)=>{
         const token_decode=jwt.verify(token,process.env.JWT_SECRET)
         req.body.userId=token_decode.id
         next()
-    } 
+    } catch (error) {
+        console.log(error);
+    res.json({success:false,message:error.message})
+    }
 }
 
 export default AuthUser
