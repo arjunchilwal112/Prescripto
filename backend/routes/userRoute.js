243deae50ae735be206cdcc65b4ecaa81ce0ff7c
bhattproject <bhattjirajeev@gmail.com>
2025-05-28 12:27:45 +0530

    Update userRoute.js
    RAZORPAY PAYMENT GATEWAY

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -12,6 +12,8 @@ UserRouter.post('/update-profile',upload.single('image'),AuthUser,updateProfile)
 UserRouter.post('/book-appointment',AuthUser,BookAppointment)
 UserRouter.get('/appointments',AuthUser,listAppointment)
 UserRouter.post('/cancel-appointment',AuthUser,CancelAppointment)
+UserRouter.post('/payment-razorpay',AuthUser,paymentRazorpay)
+UserRouter.post('/verifyRazorpay',AuthUser,verifyRaazorpay)
 
 
 export default UserRouter
