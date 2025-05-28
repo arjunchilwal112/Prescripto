1e227a1f5a27e66f4542910273502dc3336de876
mansifartiyal <mansifartiyal8@gmail.com>
2025-05-28 11:53:30 +0530

    this is authadim file

--- backend/middlewares/AuthAdmin.js
+++ backend/middlewares/AuthAdmin.js
@@ -7,10 +7,7 @@ const AuthAdmin =async (req,res,next)=>{
         if(!atoken){
             return res.json({success:false,message:"Not Authorized Login Again"})
         }
-        const token_decode=jwt.verify(atoken,process.env.JWT_SECRET)
-        if(token_decode !==process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
-            res.json({success:false,message:"Not Authorized Login Again"})
-        }
+       
         next()
     } catch (error) {
         console.log(error);
