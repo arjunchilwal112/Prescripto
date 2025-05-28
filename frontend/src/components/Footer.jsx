425726d6fbd99ce746a2e0773a59fbc29f1f6f0e
nirajbhayera4 <nirajbhayera4@gmail.com>
2025-05-28 13:13:27 +0530

    this is footer file

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,15 +11,7 @@ const Footer = () => {
                     <p className='w-full md:w:2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 {/* Center section  */}
-                <div>
-                    <p className='textxl font-medium  mb-5'>COMPANY</p>
-                    <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>Home</li>
-                        <li>About us</li>
-                        <li>Contact us</li>
-                        <li>Privacy policy</li>
-                    </ul>
-                </div>
+
                 {/* right section */}
                 <div>
                     <p className='textxl font-medium  mb-5'>GET IN TOUCH</p>
